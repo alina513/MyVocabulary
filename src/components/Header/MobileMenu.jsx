648438b1ab409b2b157aecf60cb660679a49f783
alina513 <130853290+alina513@users.noreply.github.com>
2024-06-14 00:41:46 +0300

    Update MobileMenu.jsx

--- src/components/Header/MobileMenu.jsx
+++ src/components/Header/MobileMenu.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createPortal } from 'react-dom';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Slide from '@mui/material/Slide';
 import sprite from '../../assets/sprite.svg';
@@ -25,13 +25,13 @@ const modalRoot = document.querySelector('#menu-root');
 export const MobileMenu = ({ closeMenu, isOpen }) => {
   const isMobileScreen = useMediaQuery('(max-width: 767px)');
 
-  let location = useLocation();
+  // let location = useLocation();
 
-  useEffect(() => {
-    if (isOpen) {
-      closeMenu();
-    }
-  }, [location]);
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     closeMenu();
+  //   }
+  // }, [ location]);
 
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
